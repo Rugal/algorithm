70891b3a1313739b4e31d4275a8bab894cf1f421
Rugal <rugal.bernstein.0@gmail.com>
2016-01-14 14:24:13 -0500

    SCAofBST

---
+++ dp/src/main/java/ga/rugal/leetcode/TreeNode.java
@@ -0,0 +1,20 @@
+package ga.rugal.leetcode;
+
+/**
+ *
+ * @author Rugal Bernstein
+ */
+public class TreeNode
+{
+
+    int val;
+
+    TreeNode left;
+
+    TreeNode right;
+
+    TreeNode(int x)
+    {
+        val = x;
+    }
+}
