36e8541f4f55e45fed79d10c64d7026808396b42
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2018-12-28 18:11:16 -0500

    Start over leetcode

---
+++ leetcode/src/main/java/ga/rugal/leetcode/twosum/Solution.java
@@ -0,0 +1,25 @@
+package ga.rugal.leetcode.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Leetcode two sum.<BR>
+ * https://leetcode.com/problems/two-sum/
+ *
+ * @author Rugal Bernstein
+ */
+public class Solution {
+
+  public int[] twoSum(final int[] nums, final int target) {
+    final Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; ++i) {
+      final int key = target - nums[i];
+      if (map.containsKey(key) && i != map.get(key)) {
+        return new int[]{map.get(key), i};
+      }
+      map.put(nums[i], i);
+    }
+    return null;
+  }
+}
---
+++ leetcode/src/test/java/ga/rugal/leetcode/twosum/SolutionTest.java
@@ -0,0 +1,43 @@
+package ga.rugal.leetcode.twosum;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ *
+ * @author Rugal Bernstein
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {new int[]{1, 3, 5, 7}, 12, new int[]{2, 3}},
+      {new int[]{3, 3}, 6, new int[]{0, 1}},
+      {new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}},});
+  }
+
+  @Parameter(0)
+  public int[] array;
+
+  @Parameter(1)
+  public int target;
+
+  @Parameter(2)
+  public int[] expected;
+
+  @Test
+  public void testTwoSum() {
+    Assert.assertArrayEquals(this.expected, this.solution.twoSum(this.array, this.target));
+  }
+}
