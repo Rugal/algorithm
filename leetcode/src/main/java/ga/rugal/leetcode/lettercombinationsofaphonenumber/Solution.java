68937deec0164a8c87e5575c6812357f6a7a505d
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-01-05 00:15:04 -0500

    Add letter combination

---
+++ leetcode/src/main/java/ga/rugal/leetcode/lettercombinationsofaphonenumber/Solution.java
@@ -0,0 +1,44 @@
+package ga.rugal.leetcode.lettercombinationsofaphonenumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rugal Bernstein
+ */
+public class Solution {
+
+  private static final String[] ARRAY = new String[]{
+    "",
+    "",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tuv",
+    "wxyz"};
+
+  public List<String> letterCombinations(final String digits) {
+    List<String> result = new ArrayList<>();
+    if (digits.isEmpty()) {
+      return result;
+    }
+
+    result.add("");
+    for (int i = 0; i < digits.length(); i++) {
+      final List<String> temp = new ArrayList<>();
+      final String chars = ARRAY[digits.charAt(i) - '0'];
+
+      for (int c = 0; c < chars.length(); c++) {
+        for (int j = 0; j < result.size(); j++) {
+          temp.add(result.get(j) + chars.charAt(c));
+        }
+      }
+      result = temp;
+    }
+    return result;
+  }
+}
