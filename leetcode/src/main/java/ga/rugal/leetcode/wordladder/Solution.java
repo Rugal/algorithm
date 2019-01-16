49f690f7e215147560b70d4827d02f1ee865960d
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-01-15 21:53:28 -0500

    Add Word Ladder

---
+++ leetcode/src/main/java/ga/rugal/leetcode/wordladder/Solution.java
@@ -0,0 +1,95 @@
+package ga.rugal.leetcode.wordladder;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/word-ladder
+ *
+ * @author Rugal Bernstein
+ */
+public class Solution {
+
+  static class Pair {
+
+    public String word;
+
+    public int count;
+
+    public Pair(String word, int count) {
+      this.word = word;
+      this.count = count;
+    }
+  }
+
+  private String getWord(final String word, final int i) {
+    return String.format("%s_%s", word.substring(0, i), word.substring(i + 1));
+  }
+
+  /**
+   * Create word map of words that have the same pattern, for instance all word matches AB_ will be
+   * grouped together and so forth.
+   *
+   * @param wordList
+   *
+   * @return
+   */
+  private Map<String, List<String>> build(final List<String> wordList) {
+    final Map<String, List<String>> map = new HashMap<>();
+    for (String word : wordList) {
+      for (int i = 0; i < word.length(); ++i) {
+        final String key = this.getWord(word, i);
+        final List<String> get = map.getOrDefault(key, new ArrayList<>());
+        get.add(word);
+        map.put(key, get);
+      }
+    }
+    return map;
+  }
+
+  /**
+   * start BFS with the given word map.
+   *
+   * @param beginWord
+   * @param endWord
+   * @param map
+   *
+   * @return
+   */
+  private int bfs(String beginWord, String endWord, Map<String, List<String>> map) {
+    final Deque<Pair> pairs = new ArrayDeque<>();
+    final Set<String> visited = new HashSet<>();
+    pairs.add(new Pair(beginWord, 1));
+    while (!pairs.isEmpty()) {
+      Pair pair = pairs.poll();
+      if (visited.contains(pair.word)) {
+        //we have seen this word
+        continue;
+      }
+      visited.add(pair.word);
+      if (pair.word.equals(endWord)) {
+        //Since we are using BFS, the earlier the match is, the smaller the count is
+        return pair.count;
+      }
+      for (int i = 0; i < pair.word.length(); ++i) {
+        final String key = this.getWord(pair.word, i);
+        map.getOrDefault(key, new ArrayList<>()).stream()
+          //from any unvisited neighbour words
+          .filter(neigh -> !visited.contains(neigh))
+          //since they are neighbour of us, +1 for one more step
+          .forEach(neigh -> pairs.add(new Pair(neigh, pair.count + 1)));
+      }
+    }
+    return 0;
+  }
+
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    return this.bfs(beginWord, endWord, this.build(wordList));
+  }
+}
---
+++ leetcode/src/test/java/ga/rugal/leetcode/wordladder/SolutionTest.java
@@ -0,0 +1,45 @@
+package ga.rugal.leetcode.wordladder;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author Rugal Bernstein
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {"hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log", "cog"}, 5},});
+  }
+
+  @Parameterized.Parameter(0)
+  public String start;
+
+  @Parameterized.Parameter(1)
+  public String end;
+
+  @Parameterized.Parameter(2)
+  public String[] wordList;
+
+  @Parameterized.Parameter(3)
+  public int expected;
+
+  @Test
+  public void testLadderLength() {
+    Assert.assertEquals(this.expected,
+                        this.solution.ladderLength(this.start,
+                                                   this.end,
+                                                   Arrays.asList(this.wordList)));
+  }
+}
