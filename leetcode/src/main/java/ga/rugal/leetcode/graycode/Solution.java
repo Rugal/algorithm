afb9bcb1554e4a202f5afd106955a2b7fe2a7cef
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-01-20 17:58:36 -0500

    Add gray code

---
+++ leetcode/src/main/java/ga/rugal/leetcode/graycode/Solution.java
@@ -0,0 +1,34 @@
+package ga.rugal.leetcode.graycode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/gray-code/
+ *
+ * @author Rugal Bernstein
+ */
+public class Solution {
+
+  public List<Integer> grayCode(int n) {
+    List<Integer> res = new ArrayList<>();
+    //base value
+    res.add(0);
+
+    for (int i = 1; i <= n; i++) {
+      int size = res.size(), base = 1 << (i - 1);
+      //In previous round, we have 00 and 01
+      //In current round, we add one more bit
+      //We basically add the bit by the reverse order of previous list
+
+      //example: we have 00 and 01
+      //add one bit to 01 then, 00
+      //and we have 11 and 10
+      //in totaly we have 00, 01, 11, 10
+      for (int j = size - 1; j >= 0; j--) {
+        res.add(base + res.get(j));
+      }
+    }
+    return res;
+  }
+}
---
+++ leetcode/src/test/java/ga/rugal/leetcode/graycode/SolutionTest.java
@@ -0,0 +1,39 @@
+package ga.rugal.leetcode.graycode;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author Rugal Bernstein
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {2, new int[]{0, 1, 3, 2}},});
+  }
+
+  @Parameterized.Parameter(0)
+  public int input;
+
+  @Parameterized.Parameter(1)
+  public int[] expected;
+
+  @Test
+  public void testGrayCode() {
+    final int[] toArray = this.solution.grayCode(this.input).stream()
+      .mapToInt(i -> i)
+      .toArray();
+    Assert.assertArrayEquals(this.expected, toArray);
+  }
+}
