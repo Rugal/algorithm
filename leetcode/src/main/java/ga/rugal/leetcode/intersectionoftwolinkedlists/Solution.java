9727b797c17b9a2762abef7a2386c89fbb0824f9
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-01-21 22:44:26 -0500

    Add Intersection of Two Linked Lists

---
+++ leetcode/src/main/java/ga/rugal/leetcode/intersectionoftwolinkedlists/Solution.java
@@ -0,0 +1,38 @@
+package ga.rugal.leetcode.intersectionoftwolinkedlists;
+
+import ga.rugal.leetcode.ListNode;
+
+/**
+ * https://leetcode.com/problems/intersection-of-two-linked-lists
+ *
+ * @author Rugal Bernstein
+ */
+public class Solution {
+
+  public ListNode getIntersectionNode(final ListNode headA, final ListNode headB) {
+    if (headA == null || headB == null) {
+      return null;
+    }
+    final ListNode beforeA = new ListNode(0);
+    beforeA.next = headA;
+    final ListNode beforeB = new ListNode(0);
+    beforeB.next = headB;
+
+    ListNode a = headA;
+    ListNode b = headB;
+    final boolean[] set = new boolean[2];
+    while (a != b) {
+      if (a.next == null && !set[0]) {
+        a = beforeB;
+        set[0] = true;
+      }
+      if (b.next == null && !set[1]) {
+        b = beforeA;
+        set[1] = true;
+      }
+      a = a.next;
+      b = b.next;
+    }
+    return a == b ? a : null;
+  }
+}
