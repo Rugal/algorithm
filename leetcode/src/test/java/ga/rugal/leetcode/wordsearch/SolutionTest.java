c62a7da9b59c786457e73e9a2a263dd47bd3f4ec
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-03-17 18:54:25 -0400

    Add Word Search

---
+++ leetcode/src/test/java/ga/rugal/leetcode/wordsearch/SolutionTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 rugal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.leetcode.wordsearch;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author rugal
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {new char[][]{new char[]{'A', 'B', 'C', 'E'}, new char[]{'S', 'F', 'C', 'S'}, new char[]{'A', 'D', 'E', 'E'}}, "ABCCED", true},
+      {new char[][]{new char[]{'a'}}, "a", true},}
+    );
+  }
+
+  @Parameterized.Parameter(0)
+  public char[][] board;
+
+  @Parameterized.Parameter(1)
+  public String target;
+
+  @Parameterized.Parameter(2)
+  public boolean expected;
+
+  @Test
+  public void testExist() {
+    Assert.assertEquals(this.expected, this.solution.exist(this.board, this.target));
+  }
+}
