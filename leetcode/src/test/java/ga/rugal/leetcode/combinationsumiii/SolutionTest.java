6fcfda0f39e9e4f4e1bb9a74876c080f10ad0bb0
Rugal Bernstein <this@rug.al>
2019-03-19 17:13:09 -0400

    Add Combination Sum III

---
+++ leetcode/src/test/java/ga/rugal/leetcode/combinationsumiii/SolutionTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 rugalbernstein.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.leetcode.combinationsumiii;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author rugalbernstein
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {3, 7},});
+  }
+
+  @Parameterized.Parameter(0)
+  public int k;
+
+  @Parameterized.Parameter(1)
+  public int target;
+
+  @Test
+  public void testCombinationSum3() {
+    this.solution.combinationSum3(this.k, this.target);
+  }
+}
