6c4c74b95ea53372b20e135e0dca07b80ee97806
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-03-19 00:01:59 -0400

    Add Letter Case Permutation

---
+++ leetcode/src/main/java/ga/rugal/leetcode/lettercasepermutation/Solution.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 rugal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.leetcode.lettercasepermutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/letter-case-permutation/
+ *
+ * @author rugal
+ */
+public class Solution {
+
+  private String text;
+
+  private final List<String> result = new ArrayList<>();
+
+  public List<String> letterCasePermutation(final String S) {
+    this.text = S;
+    this.backtrack(new StringBuilder(), 0);
+    return this.result;
+  }
+
+  private void backtrack(final StringBuilder temp, final int start) {
+    if (temp.length() >= this.text.length()) {
+      this.result.add(temp.toString());
+      return;
+    }
+
+    temp.append(Character.toLowerCase(this.text.charAt(start)));
+    this.backtrack(new StringBuilder(temp.toString()), start + 1);
+
+    if (!Character.isDigit(this.text.charAt(start))) {
+      temp.deleteCharAt(temp.length() - 1);
+      temp.append(Character.toUpperCase(this.text.charAt(start)));
+      this.backtrack(new StringBuilder(temp.toString()), start + 1);
+    }
+  }
+}
---
+++ leetcode/src/test/java/ga/rugal/leetcode/lettercasepermutation/SolutionTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 rugal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.leetcode.lettercasepermutation;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author rugal
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {"a1b2"}
+    });
+  }
+
+  @Parameterized.Parameter(0)
+  public String input;
+
+  @Test
+  public void testLetterCasePermutation() {
+    this.solution.letterCasePermutation(this.input);
+  }
+}
