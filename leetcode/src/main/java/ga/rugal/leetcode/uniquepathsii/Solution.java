e4cc277295b01f0cca20cfe0e60f89631c7f612b
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-04-16 01:52:59 -0400

    Add Unique Paths II

---
+++ leetcode/src/main/java/ga/rugal/leetcode/uniquepathsii/Solution.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 rugal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.leetcode.uniquepathsii;
+
+/**
+ * https://leetcode.com/problems/unique-paths-ii/
+ *
+ * @author rugal
+ */
+public class Solution {
+
+  public int uniquePathsWithObstacles(final int[][] obstacleGrid) {
+    final int row = obstacleGrid.length;
+    final int column = obstacleGrid[0].length;
+
+    if (obstacleGrid[row - 1][column - 1] == 1) {
+      return 0;
+    }
+
+    int[][] value = new int[row][column];
+    value[row - 1][column - 1] = 1;
+    for (int i = row - 2; i >= 0; i--) {
+      if (0 == obstacleGrid[i][column - 1]) {
+        value[i][column - 1] = value[i + 1][column - 1];
+      }
+    }
+
+    for (int i = column - 2; i >= 0; i--) {
+      if (0 == obstacleGrid[row - 1][i]) {
+        value[row - 1][i] = value[row - 1][i + 1];
+      }
+    }
+
+    for (int i = row - 2; i >= 0; i--) {
+      for (int j = column - 2; j >= 0; j--) {
+        if (obstacleGrid[i][j] == 1) {
+          continue;
+        }
+        value[i][j] = value[i][j + 1] + value[i + 1][j];
+      }
+    }
+
+    return value[0][0];
+  }
+}
