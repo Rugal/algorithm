a4f401c4cbf4b466faccf5b3f17245e6cdad55f7
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-04-19 04:06:11 -0400

    Add 01 Matrix

---
+++ leetcode/src/test/java/ga/rugal/leetcode/matrix01/SolutionTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 rugal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.leetcode.matrix01;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author rugal
+ */
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+  private final Solution solution = new Solution();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}},
+       new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}},
+      {new int[][]{{0, 1, 0, 1, 1}, {1, 1, 0, 0, 1}, {0, 0, 0, 1, 0}, {1, 0, 1, 1, 1}, {1, 0, 0, 0, 1}},
+       new int[][]{{0, 1, 0, 1, 2}, {1, 1, 0, 0, 1}, {0, 0, 0, 1, 0}, {1, 0, 1, 1, 1}, {1, 0, 0, 0, 1}}}
+    });
+  }
+
+  @Parameterized.Parameter(0)
+  public int[][] input;
+
+  @Parameterized.Parameter(1)
+  public int[][] expected;
+
+  @Test
+  public void testUpdateMatrix() {
+    Assert.assertArrayEquals(this.expected, this.solution.updateMatrix(this.input));
+  }
+}
