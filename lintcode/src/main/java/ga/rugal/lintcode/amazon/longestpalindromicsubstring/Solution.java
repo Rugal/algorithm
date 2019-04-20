04124bd615a983026396aff6b8a0e6058c67e5e5
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2019-04-19 23:43:15 -0400

    Use manatra algorithm

--- lintcode/src/main/java/ga/rugal/lintcode/amazon/longestpalindromicsubstring/Solution.java
+++ lintcode/src/main/java/ga/rugal/lintcode/amazon/longestpalindromicsubstring/Solution.java
@@ -23,34 +23,51 @@ package ga.rugal.lintcode.amazon.longestpalindromicsubstring;
 public class Solution {
 
   public String longestPalindrome(final String s) {
-    if (s == null || s.isEmpty()) {
+    if (s == null || s.length() == 0) {
       return "";
     }
-    int length = 1;
-    String result = s.substring(0, 1);
-    for (int i = 0; i < s.length(); ++i) {
-      if (this.isPalindrome(s, i - length - 1, i)) {
-        result = s.substring(i - length - 1, i + 1);
-        length += 2;
-        continue;
+
+    // abc => #a#b#c#
+    final String str = generateString(s);
+
+    final int[] palindrome = new int[str.length()];
+    int mid = 0, longest = 1;
+    palindrome[0] = 1;
+    for (int i = 1; i < str.length(); i++) {
+      int len = 1;
+      //from center of currently longest palindrome, if i is within that range
+      if (mid + longest > i) {
+        final int mirrorOfI = mid - (i - mid);
+        //https://www.felix021.com/blog/read.php?2040
+        len = Math.min(palindrome[mirrorOfI], mid + longest - i);
       }
-      if (this.isPalindrome(s, i - length, i)) {
-        result = s.substring(i - length, i + 1);
-        ++length;
+      for (; i + len < str.length() && i - len >= 0; len++) {
+        if (str.charAt(i - len) != str.charAt(i + len)) {
+          break;
+        }
       }
+
+      if (len > longest) {
+        longest = len;
+        mid = i;
+      }
+
+      palindrome[i] = len;
     }
-    return result;
+
+    longest = longest - 1; // remove the extra #
+    int start = (mid - 1) / 2 - (longest - 1) / 2;
+    return s.substring(start, start + longest);
   }
 
-  private boolean isPalindrome(final String s, int begin, int end) {
-    if (begin < 0) {
-      return false;
+  private String generateString(final String s) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      sb.append('#');
+      sb.append(s.charAt(i));
     }
-    while (begin < end) {
-      if (s.charAt(begin++) != s.charAt(end--)) {
-        return false;
-      }
-    }
-    return true;
+    sb.append('#');
+
+    return sb.toString();
   }
 }
