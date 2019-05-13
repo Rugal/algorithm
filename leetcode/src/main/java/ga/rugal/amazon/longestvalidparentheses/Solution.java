9802f18c8b0e92b0738647f5b6333fb7c7ad025a
Rugal Bernstein <this@rug.al>
2019-05-13 14:21:28 -0400

    Add Longest Valid Parentheses

---
+++ leetcode/src/main/java/ga/rugal/amazon/longestvalidparentheses/Solution.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 rugalbernstein.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ga.rugal.amazon.longestvalidparentheses;
+
+/**
+ * https://leetcode.com/problems/longest-valid-parentheses/
+ *
+ * @author rugalbernstein
+ */
+public class Solution {
+
+  public int longestValidParentheses(final String s) {
+    int left = 0, right = 0, maxlength = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        left++;
+      } else {
+        right++;
+      }
+      if (left == right) {
+        maxlength = Math.max(maxlength, 2 * right);
+      } else if (right >= left) {
+        left = right = 0;
+      }
+    }
+    left = right = 0;
+    for (int i = s.length() - 1; i >= 0; i--) {
+      if (s.charAt(i) == '(') {
+        left++;
+      } else {
+        right++;
+      }
+      if (left == right) {
+        maxlength = Math.max(maxlength, 2 * left);
+      } else if (left >= right) {
+        left = right = 0;
+      }
+    }
+    return maxlength;
+  }
+}
