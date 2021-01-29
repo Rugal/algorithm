37ef9e2d44bf84b6758f1ce7d6e545d5e348dbbc
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2021-01-28 21:34:00 -0500

    Finish 735. Asteroid Collision

---
+++ random/src/main/java/ga/rugal/amazon/asteroidcollision/Solution.java
@@ -0,0 +1,39 @@
+package ga.rugal.amazon.asteroidcollision;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/asteroid-collision/
+ *
+ * @author rugal
+ */
+public class Solution {
+
+  public int[] asteroidCollision(final int[] asteroids) {
+    final Stack<Integer> stack = new Stack<>();
+    for (int a : asteroids) {
+      boolean push = true;
+      while (!stack.isEmpty()) {
+        final int top = stack.peek();
+        if (top * a > 0 || top < 0) {
+          break;
+        }
+        if (top >= Math.abs(a)) {
+          push = false;
+          if (top == Math.abs(a)) {
+            stack.pop();
+          }
+          break;
+        }
+        stack.pop();
+      }
+      if (push) {
+        stack.push(a);
+      }
+    }
+
+    return stack.stream()
+      .mapToInt(Integer::intValue)
+      .toArray();
+  }
+}
