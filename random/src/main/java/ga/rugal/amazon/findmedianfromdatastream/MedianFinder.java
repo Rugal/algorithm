4bac4d879555d750cb1cfa88e4335f07d8f35047
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2021-04-07 13:53:17 -0400

    Comment 295. Find Median from Data Stream

--- random/src/main/java/ga/rugal/amazon/findmedianfromdatastream/MedianFinder.java
+++ random/src/main/java/ga/rugal/amazon/findmedianfromdatastream/MedianFinder.java
@@ -23,12 +23,14 @@ public class MedianFinder {
 
     void add(int num) {
       if (num >= val) {
+        // add to right
         if (right == null) {
           right = new TreeNode(num, this);
         } else {
           right.add(num);
         }
       } else {
+        // add to left
         if (left == null) {
           left = new TreeNode(num, this);
         } else {
@@ -40,15 +42,21 @@ public class MedianFinder {
     TreeNode next() {
       TreeNode ret;
       if (right != null) {
+        // find the smallest to the right
         ret = right;
         while (ret.left != null) {
           ret = ret.left;
         }
       } else {
+        // try to find the parent of current node
         ret = this;
         while (ret.parent.right == ret) {
+          // if current node is the right tree of its parent
+          // because its parent will be smaller than the current one
           ret = ret.parent;
         }
+        // now the current node is the left node of its parent
+        // so that the parent is larger than current one
         ret = ret.parent;
       }
       return ret;
@@ -74,31 +82,44 @@ public class MedianFinder {
 
   int n;
 
-  TreeNode root, curr;
-  // Adds a number into the data structure.
+  TreeNode root, current;
 
+  /**
+   * Adds a number into the data structure.
+   *
+   * @param num
+   */
   public void addNum(final int num) {
-    if (root == null) {
-      curr = root = new TreeNode(num, null);
-      n = 1;
+    if (this.root == null) {
+      this.current = this.root = new TreeNode(num, null);
+      this.n = 1;
       return;
     }
-    root.add(num);
-    n++;
+    this.root.add(num);
+    this.n++;
     if (n % 2 == 1) {
-      if (curr.val <= num) {
-        curr = curr.next();
+      // if odd, always set current at the very middle element
+      if (this.current.val <= num) {
+        this.current = this.current.next();
       }
-    } else if (curr.val > num) {
-      curr = curr.prev();
+    } else if (this.current.val > num) {
+      // if even, always set current at the 1 left to the middle element
+      // do not change if there it is already in this status
+      this.current = this.current.prev();
     }
   }
 
-  // Returns the median of current data stream
+  /**
+   * Returns the median of current data stream.
+   *
+   * @return
+   */
   public double findMedian() {
 
-    return n % 2 == 0
-           ? ((double) curr.next().val + curr.val) / 2
-           : curr.val;
+    return this.n % 2 == 0
+           // average of the very middle 2 elements
+           ? ((double) this.current.next().val + this.current.val) / 2
+           // if odd, just pick the middle 
+           : this.current.val;
   }
 }
